be941fbc93c001d59bc4c52c84e7d4418fca32af
Inosensey <dingcong.bae@gmail.com>
2024-05-04 09:57:46 +0800

    - Minor changes on the css of sign in page.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -11,7 +11,7 @@ import { Box, Stack } from "@chakra-ui/react";
 export default async function page() {
   const session = await getServerSession(options);
   return (
-    <Stack mt={{ sm: "8rem" }} h={{ lg: "100vh" }} color={"#fff"}>
+    <Stack mt={{ sm: "4rem" }} h={{ lg: "100vh" }} color={"#fff"}>
       <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
         <Box
           display={"flex"}
